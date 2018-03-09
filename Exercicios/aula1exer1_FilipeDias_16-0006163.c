e315e46245dbe5bdd66a2ec53d0cd1cd1e2c2c57
filypsdias <lipao.dias@hotmail.com>
2018-03-08 21:51:23 -0300

    Refactoring somethings and creating the read function to the CRUD

--- Exercicios/aula1exer1_FilipeDias_16-0006163.c
+++ Exercicios/aula1exer1_FilipeDias_16-0006163.c
@@ -18,38 +18,62 @@ void cadastraProprietario(proprietario *p){
   size_t tamanho_arq;
   int opcao = 1;
   fp = fopen("data.txt","a+");
-  if(fp != NULL){
-    tamanho_arq = ftell(fp);
-    if(tamanho_arq == 0){
-    fseek(fp, 0, SEEK_END);
-    tamanho_arq = ftell(fp);
-    }
-  }
-  if(tamanho_arq == 0){
-      fprintf(fp, "CPF           ");
-      fprintf(fp, "CHASSI                 ");
-      fprintf(fp, "\n");
-    }
+  // if(fp != NULL){
+  //   tamanho_arq = ftell(fp);
+  //   if(tamanho_arq == 0){
+  //   fseek(fp, 0, SEEK_END);
+  //   tamanho_arq = ftell(fp);
+  //   }
+  // }
+  // if(tamanho_arq == 0){
+  //     fprintf(fp, "CPF           ");
+  //     fprintf(fp, "CHASSI                 ");
+  //     fprintf(fp, "\n");
+  //   }
   printf("Insira o CPF: ");
   scanf(" %[^\n]",p->cpf);
-  // p->status = 0;
-  fprintf(fp, p->cpf);
-  fprintf(fp, "   ");
+  if(strlen(p->cpf) < 11)
+  do {
+    printf("Insira um CPF valido: ");
+    scanf(" %[^\n]",p->cpf);
+  } while(strlen(p->cpf) < 11);
+  // fprintf(fp, p->cpf);
+  // fprintf(fp, "   ");
+  fwrite(p->cpf, 11, 1, fp);
   printf("Insira o Chassi do Carro: ");
   scanf(" %[^\n]",p->veiculo.chassi);
-  fprintf(fp, p->veiculo.chassi);
-  fprintf(fp, "\n");
+  // fprintf(fp, p->veiculo.chassi);
+  fwrite(p->veiculo.chassi, 17, 1, fp);
+  fprintf(fp, "\\");
   fclose(fp);
+}
+
+void leProprietarioCPF(char cpf[11]){
+  FILE *fp;
+  int contador = 0;
+  char comparaCPF[11];
+
+  strcpy(comparaCPF, "");
+  printf("%d\n",strlen(comparaCPF));
+  fp = fopen("data.txt", "r");
+  fread(comparaCPF, 10, 1, fp);
+  // if(strcmp(comparaCPF,cpf) == 0){
+  //   printf("Iguais\n");
+  // }
+  fclose(fp);
+  printf("%s\n",comparaCPF);
+  printf("%d\n",strlen(comparaCPF));
 }
 
 int main(){
   proprietario p;
   int opcao = 1;
   while(opcao != 0){
-    system("cls");
+    // system("cls");
     printf("MENU\n");
     printf("0 - SAIR\n");
     printf("1 - Cadastrar Propietario\n");
+    printf("2 - Mostrar Dados\n");
     printf("Opcao: ");
     scanf("%d",&opcao);
     switch (opcao) {
@@ -59,6 +83,10 @@ int main(){
       case 1:
         system("cls");
         cadastraProprietario(&p);
+        break;
+      case 2:
+        system("cls");
+        leProprietarioCPF("04590615185");
         break;
       default:
         printf("Insira uma opcao valida\n");
